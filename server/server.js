863741da22ff6ff6c6a2958add49565162710eca
IJAJUL_HAQUE <20230657076@juniv.edu>
2026-08-15 20:38:08 +0600

    server

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ app.get("/api/health", (req, res) => {
 
 app.use("/api/blood-requests", require("./routes/bloodRequestRoutes"));
 app.use("/api/donors", require("./routes/donorRoutes"));
+app.use("/api/patients", require("./routes/patientRoutes"));
 
 const PORT = process.env.PORT || 3000;
 
